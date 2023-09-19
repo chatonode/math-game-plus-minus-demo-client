c364c8ba5486112e1227339758d60d0becd0e296
chatonode <chatonode@gmail.com>
2023-09-19 22:55:31 +0100

    Fixed styling of boxes with conditional backgroundImage property according to their value

--- app/components/UI/InGame/Box.tsx
+++ app/components/UI/InGame/Box.tsx
@@ -2,6 +2,13 @@ import React, { useState, memo } from 'react'
 
 import classes from './Box.module.css'
 
+import box1Image from '@/public/assets/images/boxes/box1.png'
+import box10Image from '@/public/assets/images/boxes/box10.png'
+import box100Image from '@/public/assets/images/boxes/box100.png'
+import box1000Image from '@/public/assets/images/boxes/box1000.png'
+import box10000Image from '@/public/assets/images/boxes/box10000.png'
+import box100000Image from '@/public/assets/images/boxes/box100000.png'
+
 export enum EBoxType {
   BRONZE = '1',
   SILVER = '10',
@@ -23,26 +30,33 @@ const Box = (props: TBoxProps) => {
   // const [clicksLeft, setClicksLeft] = useState<number>(props.clicksLeft || 0)
   const clicksLeft = props.clicksLeft ? parseInt(props.clicksLeft) : 0
 
-  let backgroundColor: string
+  let backgroundImage
+  // let backgroundColor
 
   switch (props.type) {
     case EBoxType.BRONZE:
-      backgroundColor = '#a05822'
+      backgroundImage = `url(${box1Image.src})`
+      // backgroundColor = '#a05822'
       break
     case EBoxType.SILVER:
-      backgroundColor = '#bec7c7'
+      backgroundImage = `url(${box10Image.src})`
+      // backgroundColor = '#bec7c7'
       break
     case EBoxType.GOLD:
-      backgroundColor = '#d6c372'
+      backgroundImage = `url(${box100Image.src})`
+      // backgroundColor = '#d6c372'
       break
     case EBoxType.DIAMOND:
-      backgroundColor = '#effffe'
+      backgroundImage = `url(${box1000Image.src})`
+      // backgroundColor = '#effffe'
       break
     case EBoxType.PLATINIUM:
-      backgroundColor = '#e5e4e2'
+      backgroundImage = `url(${box10000Image.src})`
+      // backgroundColor = '#e5e4e2'
       break
+    // TODO: 100000
     default:
-      backgroundColor = 'none'
+      backgroundImage = 'unset'
       break
   }
 
@@ -67,13 +81,11 @@ const Box = (props: TBoxProps) => {
   return (
     <button
       className={classes.box}
-      style={{ backgroundColor }}
+      style={{ backgroundImage }}
       onClick={boxClickHandler}
       disabled={clicksLeft === 0 ? true : undefined} // TODO: check & log conditions
       id={props.id}
-    >
-      <span>{props.type.toString()}</span>
-    </button>
+    />
   )
 }
 
